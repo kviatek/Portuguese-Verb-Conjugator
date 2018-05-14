cbd1920252b6ea3aa0e635f9e086293852575ecc
kviatek <36170292+kviatek@users.noreply.github.com>
2018-05-14 16:44:08 +0200

    Update enum_verbs_conjugator.py

--- enum_verbs_conjugator.py
+++ enum_verbs_conjugator.py
@@ -1,48 +1,46 @@
 from enum import Enum
 
-# This module is a collection of all constants related to portuguese verbs conjugation, 
+
+# This module is a collection of all constants related to portuguese verbs conjugation,
 # i.e. names of grammatical persons.
 
 
 class GrammaticalPersons(Enum):
-    first_person_sing = 'Eu/I'
-    second_person_sing = 'Tu/You'
-    third_person_sing = 'Ele/Ela/você/He/She/It'
-    first_person_plural = 'Nós/We'
-    second_person_plural = 'Else/Elas/vocês/They'
+    FIRST_PERSON_SING = 'Eu/I'
+    SECOND_PERSON_SING = 'Tu/You'
+    THIRD_PERSON_SING = 'Ele/Ela/você/He/She/It'
+    FIRST_PERSON_PLURAL = 'Nós/We'
+    SECOND_PERSON_PLURAL = 'Else/Elas/vocês/They'
 
 
-class GrammaticalEndings(Enum):
-
+class TenseEndings(Enum):
     # indicativo_presente_endings
 
-    indicativo_presente_endings_ar = ('o', 'as', 'a', 'amos', 'am')
-    indicativo_presente_endings_er = ('o', 'es', 'e', 'emos', 'em')
-    indicativo_presente_endings_ir = ('o', 'es', 'e', 'imos', 'em')
+    INDICATIVO_PRESENTE_ENDINGS_AR = ('o', 'as', 'a', 'amos', 'am')
+    INDICATIVO_PRESENTE_ENDINGS_ER = ('o', 'es', 'e', 'emos', 'em')
+    INDICATIVO_PRESENTE_ENDINGS_IR = ('o', 'es', 'e', 'imos', 'em')
 
     # preterito_perfeito_endings
 
-    preterito_perfeito_endings_ar = ('ei', 'aste', 'ou', 'amos', 'aram')
-    preterito_perfeito_endings_er = ('i', 'este', 'eu', 'emos', 'eram')
-    preterito_perfeito_endings_ir = ('i', 'iste', 'iu', 'imos', 'iram')
+    PRETERITO_PERFEITO_ENDINGS_AR = ('ei', 'aste', 'ou', 'amos', 'aram')
+    PRETERITO_PERFEITO_ENDINGS_ER = ('i', 'este', 'eu', 'emos', 'eram')
+    PRETERITO_PERFEITO_ENDINGS_IR = ('i', 'iste', 'iu', 'imos', 'iram')
 
     # preterito_imperfeito_endings
 
-    preterito_imperfeito_endings_ar = ('ava', 'avas', 'ava', 'ávamos', 'avam')
-    preterito_imperfeito_endings_er_ir = ('ia', 'ias', 'ia', 'íamos', 'iam')
+    PRETERITO_IMPERFEITO_ENDINGS_AR = ('ava', 'avas', 'ava', 'ávamos', 'avam')
+    PRETERITO_IMPERFEITO_ENDINGS_ER_IR = ('ia', 'ias', 'ia', 'íamos', 'iam')
 
     # futuro_simples_indicativo
 
-    futuro_simples_indicativo_endings = ('ei', 'ás', 'á', 'emos', 'ão')
+    FUTURO_SIMPLES_INDICATIVO_ENDINGS = ('ei', 'ás', 'á', 'emos', 'ão')
 
     # condicional_simples
 
-    condicional_simples_endigns = ('ia', 'ias', 'ia', 'íamos', 'iam')
-    
+    CONDICIONAL_SIMPLES_ENDIGNS = ('ia', 'ias', 'ia', 'íamos', 'iam')
+
+
 class IrregularVerbs(Enum):
-    
-    """Enum class consist of a list of all Portuguese irregular verbs"""
-    
     irregular_verbs = {
         'A': ('abster', 'acudir', 'adequar', 'aderir', 'adjazer', 'advertir', 'advir', 'afazer', 'aferir', 'agredir',
               'ansiar', 'antedar', 'antepor', 'antever', 'apor', 'aprazer', 'apropinquar', 'aspergir', 'assentir',
@@ -64,20 +62,21 @@ class IrregularVerbs(Enum):
         'F': ('fazer', 'ferir', 'fotocompor', 'fraguar', 'frigir', 'fugir'),
         'G': 'gelifazer',
         'H': 'haver',
-        'I': ('idear', 'imergir', 'impedir', 'impelir', 'impor', 'incendiar','incluir', 'indispor', 'influir',
-              'insatisfazer', 'inserir', 'interdizer', 'intermediar','interpor', 'interver', 'intervir', 'ir'),
+        'I': ('idear', 'imergir', 'impedir', 'impelir', 'impor', 'incendiar', 'incluir', 'indispor', 'influir',
+              'insatisfazer', 'inserir', 'interdizer', 'intermediar', 'interpor', 'interver', 'intervir', 'ir'),
         'J': ('jazer', 'justapor'),
         'L': ('ler', 'liquefazer'),
-        'M': ('maisquerer','maldispor', 'maldizer', 'malfazer', 'malinguar', 'malparir', 'malquerer', 'manter','mediar',
-              'medir', 'mentir', 'minguar', 'mobiliar', 'moer'),
+        'M': (
+                'maisquerer', 'maldispor', 'maldizer', 'malfazer', 'malinguar', 'malparir', 'malquerer', 'manter', 'mediar',
+                'medir', 'mentir', 'minguar', 'mobiliar', 'moer'),
         'O': ('obter', 'obvir', 'odiar', 'opor', 'ouvir'),
-        'P': ('parir', 'pedir', 'perder', 'perfazer', 'perseguir', 'persentir', 'pleitear', 'poder','polir', 'pospor',
-              'pôr', 'prazer', 'precluir', 'predispor', 'predizer', 'preferir', 'prepor','pressentir', 'pressupor',
+        'P': ('parir', 'pedir', 'perder', 'perfazer', 'perseguir', 'persentir', 'pleitear', 'poder', 'polir', 'pospor',
+              'pôr', 'prazer', 'precluir', 'predispor', 'predizer', 'preferir', 'prepor', 'pressentir', 'pressupor',
               'preterir', 'prevenir', 'prever', 'progredir', 'propor', 'prossupor', 'prover', 'provir', 'pruir',
               'puir', 'putrefazer'),
         'Q': 'querer',
-        'R': ('raer','rarefazer', 'readequar', 'reaver', 'reavir', 'recobrir', 'recompor', 'reconvir', 'redar',
-              'redispor', 'redizer','reexpedir', 'reexpor', 'refazer', 'regredir', 'reimpor', 'reindispor', 'reler',
+        'R': ('raer', 'rarefazer', 'readequar', 'reaver', 'reavir', 'recobrir', 'recompor', 'reconvir', 'redar',
+              'redispor', 'redizer', 'reexpedir', 'reexpor', 'refazer', 'regredir', 'reimpor', 'reindispor', 'reler',
               'remediar', 'remedir', 'reobter', 'reouvir', 'repedir', 'repelir', 'repor', 'repropor', 'requerer',
               'resfolegar', 'ressentir', 'reter', 'retrair', 'retranspor', 'rever', 'revir', 'rir', 'ruir'),
         'S': ('saber', 'sacudir', 'sair', 'santiguar', 'satisfazer', 'seguir', 'sentir', 'ser', 'servir', 'sobpor',
