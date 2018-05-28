0c6135fbdb9650b41797122d431c1fc058901694
kviatek <36170292+kviatek@users.noreply.github.com>
2018-05-28 17:44:58 +0200

    Update enum_verbs_conjugator.py

--- enum_verbs_conjugator.py
+++ enum_verbs_conjugator.py
@@ -3,40 +3,29 @@ from enum import Enum
 
 # This module is a collection of all names of grammatical persons and tense endings.
 
-
 class GrammaticalPersons(Enum):
     PERSONS = ('Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', "Else/Elas/vocês/They")
 
 
 class TenseEndings(Enum):
-    # indicativo_presente_endings
+    AR_ENDINGS = {'INDICATIVO PRESENTE': ('o', 'as', 'a', 'amos', 'am'),
+                  'PRETERITO_PERFEITO_INDICATIVO': ('ei', 'aste', 'ou', 'amos', 'aram'),
+                  'PRETERITO_IMPERFEITO_INDICATIVO': ('ava', 'avas', 'ava', 'ávamos', 'avam'),
+                  'PRETERITO_MAIS_QUE_PERFEITO': ('ara', 'aras', 'ara', 'áramos', 'aram'),
+                  }
 
-    INDICATIVO_PRESENTE_ENDINGS_AR = ('o', 'as', 'a', 'amos', 'am')
-    INDICATIVO_PRESENTE_ENDINGS_ER = ('o', 'es', 'e', 'emos', 'em')
-    INDICATIVO_PRESENTE_ENDINGS_IR = ('o', 'es', 'e', 'imos', 'em')
+    ENDINGS_ER = {'INDICATIVO_PRESENTE': ('o', 'es', 'e', 'emos', 'em'),
+                  'PRETERITO_PERFEITO_INDICATIVO': ('i', 'este', 'eu', 'emos', 'eram'),
+                  'PRETERITO_IMPERFEITO_INDICATIVO': ('ia', 'ias', 'ia', 'íamos', 'iam'),
+                  'PRETERITO_MAIS_QUE_PERFEITO': ('era', 'eras', 'era', 'êramos', 'eram'),
+                  }
 
-    # PAST TENSES
-    # preterito_perfeito_endings
+    ENDINGS_IR = {'INDICATIVO_PRESENTE': ('o', 'es', 'e', 'imos', 'em'),
+                  'PRETERITO_PERFEITO_INDICATIVO': ('i', 'iste', 'iu', 'imos', 'iram'),
+                  'PRETERITO_IMPERFEITO_INDICATIVO': ('ia', 'ias', 'ia', 'íamos', 'iam'),
+                  'PRETERITO_MAIS_QUE_PERFEITO': ('ira', 'iras', 'ira', 'íramos', 'iram'),
+                  }
 
-    PRETERITO_PERFEITO_INDICATIVO_ENDINGS_AR = ('ei', 'aste', 'ou', 'amos', 'aram')
-    PRETERITO_PERFEITO_INDICATIVO_ENDINGS_ER = ('i', 'este', 'eu', 'emos', 'eram')
-    PRETERITO_PERFEITO_INDICATIVO_ENDINGS_IR = ('i', 'iste', 'iu', 'imos', 'iram')
-
-    # preterito_imperfeito_endings
-
-    PRETERITO_IMPERFEITO_INDICATIVO_ENDINGS_AR = ('ava', 'avas', 'ava', 'ávamos', 'avam')
-    PRETERITO_IMPERFEITO_INDICATIVO_ENDINGS_ER_IR = ('ia', 'ias', 'ia', 'íamos', 'iam')
-
-    # preterito mais que perfeito_endings
-
-    PRETERITO_MAIS_QUE_PERFEITO_ENDINGS_AR = ('ara', 'aras', 'ara', 'áramos', 'aram')
-    PRETERITO_MAIS_QUE_PERFEITO_ENDINGS_ER = ('era', 'eras', 'era', 'êramos', 'eram')
-    PRETERITO_MAIS_QUE_PERFEITO_ENDINGS_IR = ('ira', 'iras', 'ira', 'íramos', 'iram')
-
-    # futuro_simples_indicativo
-
-    FUTURO_SIMPLES_INDICATIVO_ENDINGS = ('ei', 'ás', 'á', 'emos', 'ão')
-
-    # condicional_simples
-
-    CONDICIONAL_SIMPLES_ENDIGNS = ('ia', 'ias', 'ia', 'íamos', 'iam')
+    CONDICIONAL_AND_FUTURO_ENDINGS_COMMON_FOR_EVERY_ENDING = {
+        'FUTURO_SIMPLES_INDICATIVO': ('ei', 'ás', 'á', 'emos', 'ão'),
+        'CONDICIONAL_SIMPLES': ('ia', 'ias', 'ia', 'íamos', 'iam')}
