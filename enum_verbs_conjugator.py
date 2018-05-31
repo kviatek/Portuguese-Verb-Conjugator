2ab3587dee2b3fc5ff877ee6360315268db3fabb
kviatek <36170292+kviatek@users.noreply.github.com>
2018-05-31 10:53:20 +0200

    Update enum_verbs_conjugator.py

--- enum_verbs_conjugator.py
+++ enum_verbs_conjugator.py
@@ -4,27 +4,34 @@ from enum import Enum
 # This module is a collection of all names of grammatical persons and tense endings.
 
 class GrammaticalPersons(Enum):
-    PERSONS = ('Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', "Else/Elas/vocês/They")
+    PERSONS = ['Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', "Else/Elas/vocês/They"]
 
 
 class TenseEndings(Enum):
-    AR_ENDINGS = {'INDICATIVO PRESENTE': ('o', 'as', 'a', 'amos', 'am'),
-                  'PRETERITO_PERFEITO_INDICATIVO': ('ei', 'aste', 'ou', 'amos', 'aram'),
-                  'PRETERITO_IMPERFEITO_INDICATIVO': ('ava', 'avas', 'ava', 'ávamos', 'avam'),
-                  'PRETERITO_MAIS_QUE_PERFEITO': ('ara', 'aras', 'ara', 'áramos', 'aram'),
-                  }
 
-    ENDINGS_ER = {'INDICATIVO_PRESENTE': ('o', 'es', 'e', 'emos', 'em'),
-                  'PRETERITO_PERFEITO_INDICATIVO': ('i', 'este', 'eu', 'emos', 'eram'),
-                  'PRETERITO_IMPERFEITO_INDICATIVO': ('ia', 'ias', 'ia', 'íamos', 'iam'),
-                  'PRETERITO_MAIS_QUE_PERFEITO': ('era', 'eras', 'era', 'êramos', 'eram'),
-                  }
+    AR_ENDINGS = [['o', 'as', 'a', 'amos', 'am'],
+                  ['ei', 'aste', 'ou', 'amos', 'aram'],
+                  ['ava', 'avas', 'ava', 'ávamos', 'avam'],
+                  ['ara', 'aras', 'ara', 'áramos', 'aram'],
+                  ['e', 'es', 'e', 'emos', 'em'],
+                  ['asse', 'asses', 'asse', 'ássemos', 'assem'],
+                  ]
 
-    ENDINGS_IR = {'INDICATIVO_PRESENTE': ('o', 'es', 'e', 'imos', 'em'),
-                  'PRETERITO_PERFEITO_INDICATIVO': ('i', 'iste', 'iu', 'imos', 'iram'),
-                  'PRETERITO_IMPERFEITO_INDICATIVO': ('ia', 'ias', 'ia', 'íamos', 'iam'),
-                  'PRETERITO_MAIS_QUE_PERFEITO': ('ira', 'iras', 'ira', 'íramos', 'iram'),
-                  }
+    ENDINGS_ER = [['o', 'es', 'e', 'emos', 'em'],
+                  ['i', 'este', 'eu', 'emos', 'eram'],
+                  ['ia', 'ias', 'ia', 'íamos', 'iam'],
+                  ['era', 'eras', 'era', 'êramos', 'eram'],
+                  ['a', 'as', 'a', 'amos', 'am'],
+                  ['esse', 'esses', 'esse', 'êssemos', 'essem'],
+                  ]
+
+    ENDINGS_IR = [['o', 'es', 'e', 'imos', 'em'],
+                  ['i', 'iste', 'iu', 'imos', 'iram'],
+                  ['ia', 'ias', 'ia', 'íamos', 'iam'],
+                  ['ira', 'iras', 'ira', 'íramos', 'iram'],
+                  ['a', 'as', 'a', 'amos', 'am'],
+                  ['isse', 'isses', 'isse', 'íssemos', 'issem'],
+                  ]
 
     CONDICIONAL_AND_FUTURO_ENDINGS_COMMON_FOR_EVERY_ENDING = {
         'FUTURO_SIMPLES_INDICATIVO': ('ei', 'ás', 'á', 'emos', 'ão'),
