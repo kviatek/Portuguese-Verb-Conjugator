61dfae4252fd7795decec68b354b101485ca06e6
kviatek <36170292+kviatek@users.noreply.github.com>
2018-05-31 10:57:37 +0200

    Update __init_.py

--- __init_.py
+++ __init_.py
@@ -1,4 +1,3 @@
-import re
 from pt_conjugator import enum_verbs_conjugator
 from pt_conjugator import irregular_verbs_patterns
 from pt_conjugator.enum_verbs_conjugator import TenseEndings
@@ -59,7 +58,6 @@ def conjugate_change_last_two_letters(word, endings):
         print(tense)
 
 
-
 def conjugate_ar_ended_verb(word):
     endings = enum_verbs_conjugator.TenseEndings.AR_ENDINGS.value
     conjugate_change_last_two_letters(word, endings)
@@ -67,113 +65,12 @@ def conjugate_ar_ended_verb(word):
 
 def conjugate_er_ended_verb(word):
     endings = enum_verbs_conjugator.TenseEndings.ENDINGS_ER.value
+    conjugate_change_last_two_letters(word, endings)
 
 
 def conjugate_ir_ended_verb(word):
     endings = enum_verbs_conjugator.TenseEndings.ENDINGS_IR.value
-
-
-def conjugate_presente_indicativo(word):
-    """
-
-    :param word: Verb to be conjugated
-    :return: Conjugated verb in form of a dictionary
-    """
-    word = word.lower()
-
-    if word.endswith('ar'):
-        endings = enum_verbs_conjugator.TenseEndings.INDICATIVO_PRESENTE_ENDINGS_AR.value
-    elif word.endswith('er'):
-        endings = enum_verbs_conjugator.TenseEndings.INDICATIVO_PRESENTE_ENDINGS_ER.value
-    elif word.endswith('ir'):
-        endings = enum_verbs_conjugator.TenseEndings.INDICATIVO_PRESENTE_ENDINGS_IR.value
-
-    conjugate_change_last_two_letters(endings, word)
-
-
-def conjugate_preterito_perfeito_indicativo(word):
-    """
-
-    :param word: Verb to be conjugated
-    :return: Conjugated verb in form of a dictionary
-    """
-    word = word.lower()
-
-    if word.endswith('ar'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_PERFEITO_INDICATIVO_ENDINGS_AR.value
-    elif word.endswith('er'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_PERFEITO_INDICATIVO_ENDINGS_ER.value
-    elif word.endswith('ir'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_PERFEITO_INDICATIVO_ENDINGS_IR.value
-
-    conjugate_change_last_two_letters(endings, word)
-
-
-def conjugate_preterito_imperfeito_indicativo(word):
-    """
-
-    :param word: Verb to be conjugated
-    :return: Conjugated verb in form of a dictionary
-    """
-    word = word.lower()
-
-    if word.endswith('ar'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_IMPERFEITO_INDICATIVO_ENDINGS_AR.value
-    elif word.endswith('er') or word.endswith('ir'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_IMPERFEITO_INDICATIVO_ENDINGS_ER_IR.value
-
-    conjugate_change_last_two_letters(endings, word)
-
-
-def conjugate_preterito_mais_que_perfeito_indicativo(word):
-    """
-
-    :param word: Verb to be conjugated
-    :return: Conjugated verb in form of a dictionary
-    """
-    word = word.lower()
-
-    if word.endswith('ar'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_MAIS_QUE_PERFEITO_ENDINGS_AR.value
-    elif word.endswith('er'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_MAIS_QUE_PERFEITO_ENDINGS_ER.value
-    elif word.endswith('ir'):
-        endings = enum_verbs_conjugator.TenseEndings.PRETERITO_MAIS_QUE_PERFEITO_ENDINGS_IR.value
-
-    conjugate_change_last_two_letters(endings, word)
-
-
-def conjugate_futuro_simples_indicativo(word):
-    """
-
-    :param word: Verb to be conjugated
-    :return: Conjugated verb in form of a dictionary
-    """
-    word = word.lower()
-
-    if word == 'trazer':
-        print(enum_verbs_conjugator.FixedConjugations.FUTURO_SIMPLES_INDICATIVO_TRAZER.value)
-    elif word == 'ser':
-        print(enum_verbs_conjugator.FixedConjugations.FUTURO_SIMPLES_INDICATIVO_SER.value)
-    elif word == 'dizer':
-        print(enum_verbs_conjugator.FixedConjugations.FUTURO_SIMPLES_INDICATIVO_DIZER.value)
-    elif word == 'fazer':
-        print(enum_verbs_conjugator.FixedConjugations.FUTURO_SIMPLES_INDICATIVO_FAZER.value)
-
-
-def conjugate_condicional_simples(word):
-    """
-
-    :param word: Verb to be conjugated
-    :return: Conjugated verb in form of a dictionary
-    """
-
-    if word == 'trazer':
-        print(enum_verbs_conjugator.FixedConjugations.CONDICIONAL_TRAZER.value)
-    elif word == 'dizer':
-        print(enum_verbs_conjugator.FixedConjugations.CONDICIONAL_DIZER.value)
-    elif word == 'fazer':
-        print(enum_verbs_conjugator.FixedConjugations.CONDICIONAL_FAZER.value)
+    conjugate_change_last_two_letters(word, endings)
 
 
 if __name__ == '__main__':
@@ -194,10 +91,3 @@ if __name__ == '__main__':
 
     conjugate_ar_ended_verb(word)
 
-    # print(enum_verbs_conjugator.TenseEndings.AR_ENDINGS.value)
-    # conjugate_presente_indicativo(word)
-    # conjugate_preterito_mais_que_perfeito_indicativo(word)
-    # conjugate_preterito_perfeito_indicativo(word)
-    # conjugate_preterito_imperfeito_indicativo(word)
-    # conjugate_condicional_simples(word)
-    # conjugate_futuro_simples_indicativo(word)
