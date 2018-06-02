a774d924de063151844dbc40cc11c983a0bbc7d0
kviatek <36170292+kviatek@users.noreply.github.com>
2018-06-02 19:36:02 +0200

    Update enum_verbs_conjugator.py

--- enum_verbs_conjugator.py
+++ enum_verbs_conjugator.py
@@ -3,36 +3,39 @@ from enum import Enum
 
 # This module is a collection of all names of grammatical persons and tense endings.
 
+
 class GrammaticalPersons(Enum):
-    PERSONS = ['Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', "Else/Elas/vocês/They"]
+    PERSONS = ('Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', "Eles/Elas/vocês/They")
 
 
 class TenseEndings(Enum):
+    ENDINGS_AR = (('o', 'as', 'a', 'amos', 'am'),
+                  ('ei', 'aste', 'ou', 'amos', 'aram'),
+                  ('ava', 'avas', 'ava', 'ávamos', 'avam'),
+                  ('ara', 'aras', 'ara', 'áramos', 'aram'),
+                  ('e', 'es', 'e', 'emos', 'em'),
+                  ('asse', 'asses', 'asse', 'ássemos', 'assem'),
+                  )
 
-    ENDINGS_AR = [['o', 'as', 'a', 'amos', 'am'],
-                  ['ei', 'aste', 'ou', 'amos', 'aram'],
-                  ['ava', 'avas', 'ava', 'ávamos', 'avam'],
-                  ['ara', 'aras', 'ara', 'áramos', 'aram'],
-                  ['e', 'es', 'e', 'emos', 'em'],
-                  ['asse', 'asses', 'asse', 'ássemos', 'assem'],
-                  ]
+    ENDINGS_ER = (('o', 'es', 'e', 'emos', 'em'),
+                  ('i', 'este', 'eu', 'emos', 'eram'),
+                  ('ia', 'ias', 'ia', 'íamos', 'iam'),
+                  ('era', 'eras', 'era', 'êramos', 'eram'),
+                  ('a', 'as', 'a', 'amos', 'am'),
+                  ('esse', 'esses', 'esse', 'êssemos', 'essem'),
+                  )
 
-    ENDINGS_ER = [['o', 'es', 'e', 'emos', 'em'],
-                  ['i', 'este', 'eu', 'emos', 'eram'],
-                  ['ia', 'ias', 'ia', 'íamos', 'iam'],
-                  ['era', 'eras', 'era', 'êramos', 'eram'],
-                  ['a', 'as', 'a', 'amos', 'am'],
-                  ['esse', 'esses', 'esse', 'êssemos', 'essem'],
-                  ]
+    ENDINGS_IR = (('o', 'es', 'e', 'imos', 'em'),
+                  ('i', 'iste', 'iu', 'imos', 'iram'),
+                  ('ia', 'ias', 'ia', 'íamos', 'iam'),
+                  ('ira', 'iras', 'ira', 'íramos', 'iram'),
+                  ('a', 'as', 'a', 'amos', 'am'),
+                  ('isse', 'isses', 'isse', 'íssemos', 'issem'),
+                  )
 
-    ENDINGS_IR = [['o', 'es', 'e', 'imos', 'em'],
-                  ['i', 'iste', 'iu', 'imos', 'iram'],
-                  ['ia', 'ias', 'ia', 'íamos', 'iam'],
-                  ['ira', 'iras', 'ira', 'íramos', 'iram'],
-                  ['a', 'as', 'a', 'amos', 'am'],
-                  ['isse', 'isses', 'isse', 'íssemos', 'issem'],
-                  ]
+    CONDICIONAL_AND_FUTURO_ENDINGS_COMMON_FOR_EVERY_ENDING = (('ei', 'ás', 'á', 'emos', 'ão'),
+                                                              ('ia', 'ias', 'ia', 'íamos', 'iam'))
 
-    CONDICIONAL_AND_FUTURO_ENDINGS_COMMON_FOR_EVERY_ENDING = {
-        'FUTURO_SIMPLES_INDICATIVO': ('ei', 'ás', 'á', 'emos', 'ão'),
-        'CONDICIONAL_SIMPLES': ('ia', 'ias', 'ia', 'íamos', 'iam')}
+
+if __name__ == '__main__':
+    pass
