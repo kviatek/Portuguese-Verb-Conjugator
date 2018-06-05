cca52e16205102d4be7aed12b2560394eb368cf6
kviatek <36170292+kviatek@users.noreply.github.com>
2018-06-05 20:48:51 +0200

    Update enum_verbs_conjugator.py

--- enum_verbs_conjugator.py
+++ enum_verbs_conjugator.py
@@ -1,41 +1,60 @@
 from enum import Enum
+from collections import namedtuple
 
-
-# This module is a collection of all names of grammatical persons and tense endings.
+"""
+This module is a collection of all names of grammatical persons and tense endings.
+"""
 
 
 class GrammaticalPersons(Enum):
-    PERSONS = ('Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', "Eles/Elas/vocês/They")
+    PERSONS = ('Eu/I', 'Tu/You', 'Ele/Ela/você/He/She/It', 'Nós/We', 'Vós/You', "Eles/Elas/vocês/They")
 
 
 class TenseEndings(Enum):
-    ENDINGS_AR = (('o', 'as', 'a', 'amos', 'am'),
-                  ('ei', 'aste', 'ou', 'amos', 'aram'),
-                  ('ava', 'avas', 'ava', 'ávamos', 'avam'),
-                  ('ara', 'aras', 'ara', 'áramos', 'aram'),
-                  ('e', 'es', 'e', 'emos', 'em'),
-                  ('asse', 'asses', 'asse', 'ássemos', 'assem'),
-                  )
+    Endings = namedtuple('Endings',
+                         ('Presente_Indicativo', 'Pretérito_Perfeito', 'Pretérito_Imperfeito',
+                          'Pretérito_Mais_Que_Perfeito', 'Presente_de_Subjuntivo', 'Pretérito_Imperfeito_de_Subjuntivo',
+                          'Futuro_de_Subjuntivo'))
 
-    ENDINGS_ER = (('o', 'es', 'e', 'emos', 'em'),
-                  ('i', 'este', 'eu', 'emos', 'eram'),
-                  ('ia', 'ias', 'ia', 'íamos', 'iam'),
-                  ('era', 'eras', 'era', 'êramos', 'eram'),
-                  ('a', 'as', 'a', 'amos', 'am'),
-                  ('esse', 'esses', 'esse', 'êssemos', 'essem'),
-                  )
+    ENDINGS_AR = Endings(('o', 'as', 'a', 'amos', 'ais', 'am'),
+                         ('ei', 'aste', 'ou', 'amos', 'astes', 'aram'),
+                         ('ava', 'avas', 'ava', 'ávamos', 'áveis', 'avam'),
+                         ('ara', 'aras', 'ara', 'áramos', 'áreis', 'aram'),
+                         ('e', 'es', 'e', 'emos', 'eis', 'em'),
+                         ('asse', 'asses', 'asse', 'ássemos', 'ásseis', 'assem'),
+                         ('ar', 'ares', 'ar', 'armos', 'ardes', 'arem')
+                         )
 
-    ENDINGS_IR = (('o', 'es', 'e', 'imos', 'em'),
-                  ('i', 'iste', 'iu', 'imos', 'iram'),
-                  ('ia', 'ias', 'ia', 'íamos', 'iam'),
-                  ('ira', 'iras', 'ira', 'íramos', 'iram'),
-                  ('a', 'as', 'a', 'amos', 'am'),
-                  ('isse', 'isses', 'isse', 'íssemos', 'issem'),
-                  )
+    ENDINGS_ER = Endings(('o', 'es', 'e', 'emos', 'eis', 'em'),
+                         ('i', 'este', 'eu', 'emos', 'estes', 'eram'),
+                         ('ia', 'ias', 'ia', 'íamos', 'íeis', 'iam'),
+                         ('era', 'eras', 'era', 'êramos', 'êreis', 'eram'),
+                         ('a', 'as', 'a', 'amos', 'am'),
+                         ('esse', 'esses', 'esse', 'êssemos', 'essem'),
+                         ('er', 'eres', 'er', 'ermos', 'erdes', 'erem')
+                         )
 
-    CONDICIONAL_AND_FUTURO_ENDINGS_COMMON_FOR_EVERY_ENDING = (('ei', 'ás', 'á', 'emos', 'ão'),
-                                                              ('ia', 'ias', 'ia', 'íamos', 'iam'))
+    ENDINGS_IR = Endings(('o', 'es', 'e', 'imos', 'em'),
+                         ('i', 'iste', 'iu', 'imos', 'iram'),
+                         ('ia', 'ias', 'ia', 'íamos', 'iam'),
+                         ('ira', 'iras', 'ira', 'íramos', 'iram'),
+                         ('a', 'as', 'a', 'amos', 'am'),
+                         ('isse', 'isses', 'isse', 'íssemos', 'issem'),
+                         ('ir', 'ires', 'ir', 'irmos', 'irdes', 'irem')
+                         )
+
+    CONDITIONAL_MOOD_AND_FUTURE_INDICATIVE_MOOD = namedtuple('conditional_and_future_simple',
+                                                             'conditional future_simple')
+
+    CONDITIONAL_AND_FUTURE_ENDINGS = CONDITIONAL_MOOD_AND_FUTURE_INDICATIVE_MOOD(
+        ('ei', 'ás', 'á', 'emos', 'ão'), ('ia', 'ias', 'ia', 'íamos', 'iam'))
+
+    IMPERATIVE_MOOD = namedtuple('imperative_mood', 'affirmative_imperative negative_imperative')
+
+    IMPERATIVE_ENDINGS = IMPERATIVE_MOOD()
 
 
 if __name__ == '__main__':
     pass
+
+
